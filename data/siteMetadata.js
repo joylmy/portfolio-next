746ae19da0c5097f7e51a03ab5b2c92f9bb62a9a
fengxiakira <limengyuanwinnie@163.com>
2024-06-11 23:11:14 -0400

    update metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,7 +7,7 @@ const siteMetadata = {
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: '',
+  siteUrl: 'https://www.google.com',
   siteRepo: 'https://github.com/joylmy/portfolio-next',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
