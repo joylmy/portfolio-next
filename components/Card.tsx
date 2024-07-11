cf3d119049b382a0e5a11f19aa30b3445dcfd800
fengxiakira <limengyuanwinnie@163.com>
2024-07-10 22:50:52 -0400

    add some css to card

--- components/Card.tsx
+++ components/Card.tsx
@@ -9,19 +9,30 @@ const Card = ({ title, description, imgSrc, href }) => {
 
 
   return (
-  <div 
-    className={`md max-w-[544px] p-4 md:w-1/2 transition-transform duration-300 ${isHovered ? 'transform -translate-y-1 ':''}`}
-    onMouseEnter={()=>setIsHovered(true)}
-    onMouseLeave={()=>setIsHovered(false)}
-  >
     <div
-      className={`${
-        imgSrc && 'h-full'
-      }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
+      className={`md max-w-[544px] p-4  md:w-1/2 `}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
-      {imgSrc &&
-        (href ? (
-          <Link href={href} aria-label={`Link to ${title}`}>
+      <div
+        className={`${
+          imgSrc && 'h-full'
+        }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 transition-transform duration-300 dark:border-gray-700 ${
+          isHovered ? '-translate-y-1 transform border-2 border-primary-500 border-opacity-60 ' : ''
+        }`}
+      >
+        {imgSrc &&
+          (href ? (
+            <Link href={href} aria-label={`Link to ${title}`}>
+              <Image
+                alt={title}
+                src={imgSrc}
+                className="object-cover object-center md:h-36 lg:h-48"
+                width={544}
+                height={306}
+              />
+            </Link>
+          ) : (
             <Image
               alt={title}
               src={imgSrc}
@@ -29,40 +40,31 @@ const Card = ({ title, description, imgSrc, href }) => {
               width={544}
               height={306}
             />
-          </Link>
-        ) : (
-          <Image
-            alt={title}
-            src={imgSrc}
-            className="object-cover object-center md:h-36 lg:h-48"
-            width={544}
-            height={306}
-          />
-        ))}
-      <div className="p-6">
-        <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
-          {href ? (
-            <Link href={href} aria-label={`Link to ${title}`}>
-              {title}
+          ))}
+        <div className="p-6">
+          <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
+            {href ? (
+              <Link href={href} aria-label={`Link to ${title}`}>
+                {title}
+              </Link>
+            ) : (
+              title
+            )}
+          </h2>
+          <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>
+          {href && (
+            <Link
+              href={href}
+              className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+              aria-label={`Link to ${title}`}
+            >
+              Learn more &rarr;
             </Link>
-          ) : (
-            title
           )}
-        </h2>
-        <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>
-        {href && (
-          <Link
-            href={href}
-            className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            aria-label={`Link to ${title}`}
-          >
-            Learn more &rarr;
-          </Link>
-        )}
+        </div>
       </div>
     </div>
-  </div>
-  );
+  )
 
 }
 
