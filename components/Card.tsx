71276dd4fa1a77508696e91f40ac8853fb492877
fengxiakira <limengyuanwinnie@163.com>
2024-07-14 22:36:16 -0400

    update card visual

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,7 +3,7 @@ import Image from './Image'
 import Link from './Link'
 import React, { useState } from 'react'
 
-const Card = ({ title, description, imgSrc, href }) => {
+const Card = ({ title, description, imgSrc, href, tags }) => {
   const [isHovered, setIsHovered] = useState(false)
 
 
@@ -18,7 +18,9 @@ const Card = ({ title, description, imgSrc, href }) => {
         className={`${
           imgSrc && 'h-full'
         }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 transition-transform duration-300 dark:border-gray-700 ${
-          isHovered ? '-translate-y-1 transform border-2 border-primary-500 border-opacity-60 ' : ''
+          isHovered
+            ? 'dark:border-darkPrimary-500 -translate-y-1 transform border-2 border-primary-500 border-opacity-60'
+            : ''
         }`}
       >
         {imgSrc &&
@@ -41,7 +43,7 @@ const Card = ({ title, description, imgSrc, href }) => {
               height={306}
             />
           ))}
-        <div className="p-6">
+        <div className="p-6 ">
           <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
             {href ? (
               <Link href={href} aria-label={`Link to ${title}`}>
@@ -52,15 +54,27 @@ const Card = ({ title, description, imgSrc, href }) => {
             )}
           </h2>
           <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>
-          {href && (
-            <Link
-              href={href}
-              className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-              aria-label={`Link to ${title}`}
-            >
-              Learn more &rarr;
-            </Link>
-          )}
+          <div className="mt-4 flex space-x-2">
+            {tags &&
+              tags.map((t) => {
+                return (
+                  <div className="dark:bg-darkPrimary-500 rounded-md bg-primary-300 px-2 py-1 text-sm font-medium text-gray-500 dark:text-slate-100">
+                    {t}
+                  </div>
+                )
+              })}
+          </div>
+          <div className="mt-2">
+            {href && (
+              <Link
+                href={href}
+                className="dark:text-darkPrimary-400 dark:hover:text-darkPrimary-600 text-base font-medium leading-6 text-primary-500 hover:text-primary-600"
+                aria-label={`Link to ${title}`}
+              >
+                Learn more &rarr;
+              </Link>
+            )}
+          </div>
         </div>
       </div>
     </div>
