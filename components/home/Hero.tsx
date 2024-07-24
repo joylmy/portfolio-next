74434b84e0b6ed3b0224131ce28a88b6063f2c53
fengxiakira <limengyuanwinnie@163.com>
2024-07-23 23:00:13 -0400

    add text highlight

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,26 +1,68 @@
+
 'use client'
-import React, { useEffect } from 'react'
+import React from 'react'
 import Image from 'next/image'
+import { motion } from 'framer-motion'
+
+const TEXT = [
+  {
+    text: 'swim',
+  },
+  {
+    text: 'take photos',
+  },
+  {
+    text: 'rescue cats',
+  },
+  {
+    text: 'coordinate music',
+  },
+]
 
 const Hero = () => {
   return (
     <div className="space-y-5 md:my-5">
       {/* Text and image */}
-      <div className="flex flex-col-reverse gap-8 md:flex-row ">
-        {/* Head - motion effects*/}
+      <div className="flex flex-col-reverse gap-8 md:flex-row">
+        {/* Head - motion effects */}
         <div className="space-y-5">
           {/* Text */}
           <div className="space-y-5">
             <div className="space-y-2 text-5xl font-bold">
               <h1>🐯 Hi I’m Mengyuan Li</h1>
-              <h1>aka Joy</h1>
+              <h1>
+                aka{' '}
+                <motion.span
+                  initial={{
+                    backgroundSize: '0% 100%',
+                  }}
+                  animate={{
+                    backgroundSize: '100% 100%',
+                  }}
+                  transition={{
+                    duration: 2,
+                    ease: 'linear',
+                    delay: 0.3,
+                  
+                  }}
+                  style={{
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'left center',
+                    display: 'inline',
+                  }}
+                  className=
+                    "relative inline-block rounded-lg  bg-gradient-to-r text-white from-primary-300 to-indigo-300 px-1 pb-1 dark:from-darkPrimary-500 dark:to-amber-600 "               
+                >
+                  Joy
+                </motion.span>
+              </h1>
             </div>
             <div className="space-y-3 text-3xl font-normal">
-              <h1>By day, I build cloud solutions </h1>
+              <h1>By day, I build cloud solutions</h1>
               <h1>By night, I swim / take photos / rescue cats / coordinate music</h1>
             </div>
           </div>
-          {/* Timzone */}
+          {/* Timezone */}
           <div className="text-muted-foreground text-base">Toronto • UTC -5</div>
         </div>
 
@@ -42,3 +84,4 @@ const Hero = () => {
 }
 
 export default Hero
+
