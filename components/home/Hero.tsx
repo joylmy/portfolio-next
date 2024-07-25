b7c7fa30ec12683df0658d29b2e036af4cccd269
fengxiakira <limengyuanwinnie@163.com>
2024-07-24 23:35:12 -0400

    finish hero part

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,34 +1,60 @@
 
 'use client'
-import React from 'react'
+import React,{useEffect} from 'react'
 import Image from 'next/image'
-import { motion } from 'framer-motion'
+import { motion,useAnimate } from 'framer-motion'
 
 const TEXT = [
   {
     text: 'swim',
+    className: 'bg-clip-text text-transparent bg-gradient-to-r from-[#ff1835] to-[#ffc900]',
   },
   {
     text: 'take photos',
+    className: 'bg-clip-text text-transparent bg-gradient-to-r from-[#fca311] to-[#00b4d8]',
   },
   {
     text: 'rescue cats',
+    className: 'bg-clip-text text-transparent bg-gradient-to-r from-[#f72585] to-[#4cc9f0]',
   },
   {
     text: 'coordinate music',
+    className: 'bg-clip-text text-transparent bg-gradient-to-r from-[#9381ff] to-[#ffd8be]',
+  },
+  {
+    text: 'swim',
+    className: 'bg-clip-text text-transparent bg-gradient-to-r from-[#ff1835] to-[#ffc900]',
   },
 ]
 
 const Hero = () => {
+  const [scope, animate] = useAnimate()
+
+  useEffect(() => {
+    void animate(
+      [
+        [scope.current, { y: '0%' }, { duration: 0.3 }],
+        [scope.current, { y: '-20%' }, { duration: 0.3, at: '+1.3' }],
+        [scope.current, { y: '-40%' }, { duration: 0.3, at: '+1.3' }],
+        [scope.current, { y: '-60%' }, { duration: 0.3, at: '+1.3' }],
+        [scope.current, { y: '-80%' }, { duration: 0.3, at: '+1.3' }],
+      ],
+      {
+        repeat: Number.POSITIVE_INFINITY,
+      }
+    )
+  }, [animate, scope])
+
+
   return (
-    <div className="space-y-5 md:my-5">
+    <div className="space-y-5 md:my-5 ">
       {/* Text and image */}
-      <div className="flex flex-col-reverse gap-8 md:flex-row">
+      <div className="flex flex-col-reverse gap-8 md:flex-row justify-between ">
         {/* Head - motion effects */}
         <div className="space-y-5">
           {/* Text */}
           <div className="space-y-5">
-            <div className="space-y-2 text-5xl font-bold">
+            <div className="space-y-4 text-4xl md:text-5xl font-bold ">
               <h1>🐯 Hi I’m Mengyuan Li</h1>
               <h1>
                 aka{' '}
@@ -43,23 +69,33 @@ const Hero = () => {
                     duration: 2,
                     ease: 'linear',
                     delay: 0.3,
-                  
                   }}
                   style={{
                     backgroundRepeat: 'no-repeat',
                     backgroundPosition: 'left center',
                     display: 'inline',
                   }}
-                  className=
-                    "relative inline-block rounded-lg  bg-gradient-to-r text-white from-primary-300 to-indigo-300 px-1 pb-1 dark:from-darkPrimary-500 dark:to-amber-600 "               
+                  className="relative inline-block rounded-lg  bg-gradient-to-r from-primary-300 to-indigo-300 px-1 pb-1 text-white dark:from-darkPrimary-500 dark:to-amber-600 "
                 >
                   Joy
                 </motion.span>
               </h1>
             </div>
-            <div className="space-y-3 text-3xl font-normal">
+            <div className="space-y-4 text-2xl font-normal md:text-3xl">
               <h1>By day, I build cloud solutions</h1>
-              <h1>By night, I swim / take photos / rescue cats / coordinate music</h1>
+              <h1 >
+                By night, I {''}
+                <div className="inline-grid h-8 overflow-hidden ">
+                  <div ref={scope}>
+                    {TEXT.map(({ text, className }, i) => (
+                      // eslint-disable-next-line @eslint-react/no-array-index-key -- it's static
+                      <div className={className} key={i}>
+                        {text}
+                      </div>
+                    ))}
+                  </div>
+                </div>{' '}
+              </h1>
             </div>
           </div>
           {/* Timezone */}
