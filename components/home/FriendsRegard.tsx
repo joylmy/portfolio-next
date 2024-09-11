4b923e2a241c2da60a117aa066578824e9133fb9
fengxiakira <limengyuanwinnie@163.com>
2024-09-10 23:10:31 -0400

    FriendsRgard Initial

---
+++ components/home/FriendsRegard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FriendsRegard = () => {
+  const EricaRegard =
+    'Resiliency is a recurring trait I see in my interactions with Mengyuan. Whether it’s a small or large goal, she sees it through to the end when she sets her mind to it.'
+  
+  const MarkRegard = "Willingness and ability to learn new technologies and adapt to changing requirements 2. Strong interpersonal skills to work effectively within a team and communicate clearly with stakeholders 3. Patience to troubleshoot issues and persistence to overcome obstacles."
+  
+    return (
+    <div>FriendsRegard</div>
+  )
+}
+
+export default FriendsRegard
